185be1c927e75020363175de9de98c2807aa0ca4
DLW <dlw@MacBook-Pro-de-DLW.local>
2023-04-29 01:36:31 -0400

    finalizando parte de calculo do preco e mostrando no site

--- src/lot.js
+++ src/lot.js
@@ -1,13 +1,33 @@
 "use strict";
 // valor do m2
 var valorM2 = 1000;
+function formatarPreco(preco) {
+    // Usar o tolocalestring com esses atributos para o valor resultante sair como R$ XX,XX)
+    return preco.toLocaleString("pt-br", { style: "currency", currency: "BRL" });
+}
 // Calculo area em m2 = largura * profundidade
-function calculoArea(l, p) {
-    return l * p;
+function calculoArea(largura, comprimento) {
+    return largura * comprimento;
 }
 // Calculo do preco = area * valor do m2
-function calculoPrecoM2(calculoArea) {
-    return calculoArea * valorM2;
+function calculoPrecoM2(area) {
+    var preco = area * valorM2;
+    return formatarPreco(preco);
 }
 // Pegar dados do input e atualizar o valor
-console.log(calculoPrecoM2(1));
+var form = document.querySelector("#calculadora");
+form.addEventListener("submit", function (element) {
+    element.preventDefault();
+    // pegar os valores dos inputs
+    var largura = form.largura.value;
+    var comprimento = form.comprimento.value;
+    // calcular area
+    var area = calculoArea(largura, comprimento);
+    // calcular preco
+    var preco = calculoPrecoM2(area);
+    // atualizar paragrafo
+    var valorElement = document.querySelector("#valor");
+    valorElement.innerText = preco;
+});
+var area = calculoArea(2, 10);
+console.log(calculoPrecoM2(area));
--- src/lot.ts
+++ src/lot.ts
@@ -1,16 +1,44 @@
 // valor do m2
 const valorM2: number = 1000;
 
+function formatarPreco(preco: number) {
+  // Usar o tolocalestring com esses atributos para o valor resultante sair como R$ XX,XX)
+  return preco.toLocaleString("pt-br", { style: "currency", currency: "BRL" });
+}
+
 // Calculo area em m2 = largura * profundidade
-function calculoArea(l: number, p: number): number {
-  return l * p;
+function calculoArea(largura: number, comprimento: number): number {
+  return largura * comprimento;
 }
 
 // Calculo do preco = area * valor do m2
-function calculoPrecoM2(area: number): number {
-  return area * valorM2;
+function calculoPrecoM2(area: number) {
+  const preco: number = area * valorM2;
+
+  return formatarPreco(preco);
 }
 
 // Pegar dados do input e atualizar o valor
+const form = document.querySelector("#calculadora") as HTMLFormElement;
 
-console.log(calculoPrecoM2(1));
+form.addEventListener("submit", (element) => {
+  element.preventDefault();
+
+  // pegar os valores dos inputs
+  let largura: number = form.largura.value;
+  let comprimento: number = form.comprimento.value;
+
+  // calcular area
+  let area = calculoArea(largura, comprimento);
+
+  // calcular preco
+  let preco = calculoPrecoM2(area);
+
+  // atualizar paragrafo
+  const valorElement = document.querySelector("#valor") as HTMLParagraphElement;
+  valorElement.innerText = preco;
+});
+
+const area = calculoArea(2, 10);
+
+console.log(calculoPrecoM2(area));
